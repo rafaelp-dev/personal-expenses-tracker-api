7c338ead02b72e276b636270fbc8eb7bed61fb92
Rafaelp05 <pereiraborgesrafael05@gmail.com>
2025-08-12 21:22:40 -0300

    Initial commit

---
+++ src/main/java/com/rafael/personalexpensetracker/personal_expenses_tracker/PersonalExpensesTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.personalexpensetracker.personal_expenses_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalExpensesTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalExpensesTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafael/personalexpensetracker/personal_expenses_tracker/PersonalExpensesTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.personalexpensetracker.personal_expenses_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalExpensesTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
