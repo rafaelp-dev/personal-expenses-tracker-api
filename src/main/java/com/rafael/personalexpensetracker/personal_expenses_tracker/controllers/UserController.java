d1ecb0deb40bec6fbd92d2f41d1bc7d0ce7d7cdf
Rafaelp05 <pereiraborgesrafael05@gmail.com>
2025-09-02 20:03:42 -0300

    Add getAllUsers method in UserService and UserController for retrieving all users

--- src/main/java/com/rafael/personalexpensetracker/personal_expenses_tracker/controllers/UserController.java
+++ src/main/java/com/rafael/personalexpensetracker/personal_expenses_tracker/controllers/UserController.java
@@ -1,8 +1,23 @@
 package com.rafael.personalexpensetracker.personal_expenses_tracker.controllers;
+import com.rafael.personalexpensetracker.personal_expenses_tracker.entities.UserEntity;
+import com.rafael.personalexpensetracker.personal_expenses_tracker.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserEntity>> getAllUsers(){
+        return userService.getAllUsers();
+    }
 }
--- src/main/java/com/rafael/personalexpensetracker/personal_expenses_tracker/services/UserService.java
+++ src/main/java/com/rafael/personalexpensetracker/personal_expenses_tracker/services/UserService.java
@@ -1,6 +1,22 @@
 package com.rafael.personalexpensetracker.personal_expenses_tracker.services;
 
+import com.rafael.personalexpensetracker.personal_expenses_tracker.entities.UserEntity;
+import com.rafael.personalexpensetracker.personal_expenses_tracker.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
 
+    @Autowired
+    UserRepository userRepository;
 
+    public ResponseEntity getAllUsers(){
+        List<UserEntity> userEntityList = userRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(userEntityList);
+    }
 }
