609ba86d594914e76439dda84e8f299553b62ea2
Rafaelp05 <pereiraborgesrafael05@gmail.com>
2025-09-02 19:13:01 -0300

    Add getAllUsers method to UserController for retrieving all users

--- src/main/java/com/rafael/personalexpensetracker/personal_expenses_tracker/controllers/UserController.java
+++ src/main/java/com/rafael/personalexpensetracker/personal_expenses_tracker/controllers/UserController.java
@@ -4,7 +4,6 @@ import com.rafael.personalexpensetracker.personal_expenses_tracker.entities.User
 import com.rafael.personalexpensetracker.personal_expenses_tracker.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,4 +14,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping
+    public ResponseEntity getAllUsers(){
+        List<UserEntity> userEntityList = userRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(userEntityList);
+    }
 }
