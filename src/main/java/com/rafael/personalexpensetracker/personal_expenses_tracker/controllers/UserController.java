11c279bb1413202f90726a345fcea890b3018721
Rafaelp05 <pereiraborgesrafael05@gmail.com>
2025-09-02 19:09:36 -0300

    Add UserController for managing user-related operations

---
+++ src/main/java/com/rafael/personalexpensetracker/personal_expenses_tracker/controllers/UserController.java
@@ -0,0 +1,18 @@
+package com.rafael.personalexpensetracker.personal_expenses_tracker.controllers;
+
+import com.rafael.personalexpensetracker.personal_expenses_tracker.entities.UserEntity;
+import com.rafael.personalexpensetracker.personal_expenses_tracker.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+}
