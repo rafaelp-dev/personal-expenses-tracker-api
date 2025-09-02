6aad4b8bb30c6be71266e60c683d2dc2be12cb7f
Rafaelp05 <pereiraborgesrafael05@gmail.com>
2025-09-02 19:23:28 -0300

    Remove unused imports and commented-out code from UserController

--- src/main/java/com/rafael/personalexpensetracker/personal_expenses_tracker/controllers/UserController.java
+++ src/main/java/com/rafael/personalexpensetracker/personal_expenses_tracker/controllers/UserController.java
@@ -1,26 +1,8 @@
 package com.rafael.personalexpensetracker.personal_expenses_tracker.controllers;
-
-import com.rafael.personalexpensetracker.personal_expenses_tracker.entities.UserEntity;
-import com.rafael.personalexpensetracker.personal_expenses_tracker.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-
-    @Autowired
-    UserRepository userRepository;
-
-    @GetMapping
-    public ResponseEntity getAllUsers(){
-        List<UserEntity> userEntityList = userRepository.findAll();
-        return ResponseEntity.status(HttpStatus.OK).body(userEntityList);
-    }
 }
