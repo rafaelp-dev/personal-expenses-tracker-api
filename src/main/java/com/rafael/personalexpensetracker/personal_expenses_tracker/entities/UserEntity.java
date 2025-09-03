013880beff66b4ae40ea145021a2c3278c0dc71e
Rafaelp05 <pereiraborgesrafael05@gmail.com>
2025-09-03 16:02:38 -0300

    config

--- src/main/java/com/rafael/personalexpensetracker/personal_expenses_tracker/entities/UserEntity.java
+++ src/main/java/com/rafael/personalexpensetracker/personal_expenses_tracker/entities/UserEntity.java
@@ -1,6 +1,7 @@
 package com.rafael.personalexpensetracker.personal_expenses_tracker.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +14,8 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userId;
+
+    @NotBlank(message = "Nome do usuário não pode ser vazio ou nulo.")
     private String name;
 
     public UserEntity(){}
