b075e521ae1a18eeb1198a177bb0c8fb9133e895
Rafaelp05 <pereiraborgesrafael05@gmail.com>
2025-09-22 19:57:42 -0300

    Add ExpenseController to handle expense-related requests

---
+++ src/main/java/com/rafael/personalexpensetracker/personal_expenses_tracker/controllers/ExpenseController.java
@@ -0,0 +1,27 @@
+package com.rafael.personalexpensetracker.personal_expenses_tracker.controllers;
+
+import com.rafael.personalexpensetracker.personal_expenses_tracker.entities.ExpenseEntity;
+import com.rafael.personalexpensetracker.personal_expenses_tracker.services.ExpenseService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/expenses")
+public class ExpenseController {
+    private final ExpenseService expenseService;
+
+    public ExpenseController(ExpenseService expenseService){
+        this.expenseService = expenseService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ExpenseEntity>> getAllExpenses(){
+        List<ExpenseEntity> expenses = expenseService.getAllExpenses();
+
+        return ResponseEntity.ok().body(expenses);
+    }
+}
