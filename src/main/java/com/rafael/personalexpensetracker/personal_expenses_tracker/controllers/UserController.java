2cfc51f58119f5a5d4d73ceba0018fae0ecd08ac
Rafaelp05 <pereiraborgesrafael05@gmail.com>
2025-09-23 19:05:57 -0300

    config

--- src/main/java/com/rafael/personalexpensetracker/personal_expenses_tracker/controllers/UserController.java
+++ src/main/java/com/rafael/personalexpensetracker/personal_expenses_tracker/controllers/UserController.java
@@ -13,9 +13,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
 
-    @Autowired
-    UserService userService;
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
 
     @GetMapping
     public ResponseEntity<List<UserEntity>> getAllUsers(){
